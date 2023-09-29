9971af302ac48e0a62066838d7724706b30d4a0e
Rocio Benitez <rociobenitez2403@gmail.com>
2023-09-29 16:52:57 +0200

    Update ivr_summary SQL

--- create_table_ivr_summary.sql
+++ create_table_ivr_summary.sql
@@ -2,40 +2,48 @@ CREATE OR REPLACE TABLE `practica-keepcoding-400117.keepcoding.ivr_summary` AS
 SELECT 
   d.calls_ivr_id AS ivr_id,
   d.calls_phone_number AS phone_number,
-  d.calls_ivr_result AS ivr_result,
-  CASE
-    WHEN d.calls_vdn_label LIKE 'FRONT%' THEN 'ATC'
-    WHEN d.calls_vdn_label LIKE 'TECH%' THEN 'TECH'
-    WHEN d.calls_vdn_label = 'ABSORPTION' THEN 'ABSORPTION'
-    ELSE 'RESTO'
-  END AS vdn_aggregation,
-  d.calls_start_date AS start_date,
-  d.calls_end_date AS end_date,
-  d.calls_total_duration AS total_duration,
-  d.calls_customer_segment AS customer_segment,
-  d.calls_ivr_language AS ivr_language,
-  d.calls_steps_module AS steps_module,
-  d.calls_module_aggregation AS module_aggregation,
-  d.document_type,
-  d.document_identification,
-  d.customer_phone,
-  d.billing_account_id,
-  CASE
-    WHEN d.module_name = 'AVERIA_MASIVA' THEN 1
-    ELSE 0
-  END AS masiva_lg,
-  CASE
-    WHEN (d.step_name = 'CUSTOMERINFOBYPHONE.TX' 
-        AND NULLIF(d.step_description_error, 'NULL') IS NULL) THEN 1
-    ELSE 0
-  END AS info_by_phone_lg,
-  CASE
-    WHEN (d.step_name = 'CUSTOMERINFOBYDNI.TX'
-        AND NULLIF(d.step_description_error, 'NULL') IS NULL) THEN 1
-    ELSE 0
-  END AS info_by_dni_lg,
-IF(prev_call_date IS NOT NULL, 1, 0) AS repeated_phone_24H,
-IF(next_call_date IS NOT NULL, 1, 0) AS cause_recall_phone_24H
+  MAX(d.calls_ivr_result) AS ivr_result,
+  MAX(
+    CASE
+      WHEN d.calls_vdn_label LIKE 'FRONT%' THEN 'ATC'
+      WHEN d.calls_vdn_label LIKE 'TECH%' THEN 'TECH'
+      WHEN d.calls_vdn_label = 'ABSORPTION' THEN 'ABSORPTION'
+      ELSE 'RESTO'
+    END
+  ) AS vdn_aggregation,
+  MAX(d.calls_start_date) AS start_date,
+  MAX(d.calls_end_date) AS end_date,
+  SUM(d.calls_total_duration) AS total_duration,
+  MAX(d.calls_customer_segment) AS customer_segment,
+  MAX(d.calls_ivr_language) AS ivr_language,
+  MAX(d.calls_steps_module) AS steps_module,
+  MAX(d.calls_module_aggregation) AS module_aggregation,
+  MAX(d.document_type) AS document_type,
+  MAX(d.document_identification) AS document_identification,
+  MAX(d.customer_phone) AS customer_phone,
+  MAX(d.billing_account_id) AS billing_account_id,
+  MAX(
+    CASE
+      WHEN d.module_name = 'AVERIA_MASIVA' THEN 1
+      ELSE 0
+    END
+  ) AS masiva_lg,
+  MAX(
+    CASE
+      WHEN (d.step_name = 'CUSTOMERINFOBYPHONE.TX' 
+          AND NULLIF(d.step_description_error, 'NULL') IS NULL) THEN 1
+      ELSE 0
+    END
+  ) AS info_by_phone_lg,
+  MAX(
+    CASE
+      WHEN (d.step_name = 'CUSTOMERINFOBYDNI.TX'
+          AND NULLIF(d.step_description_error, 'NULL') IS NULL) THEN 1
+      ELSE 0
+    END
+  ) AS info_by_dni_lg,
+  MAX(IF(prev_call_date IS NOT NULL, 1, 0)) AS repeated_phone_24H,
+  MAX(IF(next_call_date IS NOT NULL, 1, 0)) AS cause_recall_phone_24H
 FROM (
   SELECT
     calls_ivr_id,
@@ -48,4 +56,5 @@ FROM (
     FROM `practica-keepcoding-400117.keepcoding.ivr_detail`
 ) AS CallsWithTimestamp
 JOIN `practica-keepcoding-400117.keepcoding.ivr_detail` AS d
-ON CallsWithTimestamp.calls_ivr_id = d.calls_ivr_id;
+ON CallsWithTimestamp.calls_ivr_id = d.calls_ivr_id
+GROUP BY ivr_id, phone_number;
